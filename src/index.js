8f8a3cbb9eae5c5a8411a087f669e01feefed222
João Marcos Soré de Morais <jaumcj@hotmail.com>
2019-11-03 12:35:12 -0300

    Upload de Imagens e Justes no front

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import { init_menu } from "./js/menu";
 import { init_carousel } from "./js/carousel";
 import { init_video } from "./js/video";
 
+import { init_animation } from "./js/animation";
+
 function init() {
   init_menu();
   init_carousel();
   init_video();
+  init_animation();
 }
 init();
 
--- src/js/animation.js
+++ src/js/animation.js
@@ -1,7 +1,22 @@
 import ScrollReveal from "scrollreveal";
 
 export function init_animation() {
-  ScrollReveal().reveal(".inicio", {
+  ScrollReveal().reveal("#inicial", {
+    delay: 375,
+    duration: 500,
+    opacity: 0
+  });
+  ScrollReveal().reveal("#video", {
+    delay: 375,
+    duration: 500,
+    opacity: 0
+  });
+  ScrollReveal().reveal("#o-que-fazemos .row", {
+    delay: 375,
+    duration: 500,
+    opacity: 0
+  });
+  ScrollReveal().reveal("#footer", {
     delay: 375,
     duration: 500,
     opacity: 0
